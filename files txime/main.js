6e094bc645ce042a7e67b594d59b3ef9468580b7
nanot <[carlitosjzuazo@gmail.com]>
2022-03-17 21:08:14 +0100

    esqueleto html y js posterior a la tutoría del 17/03

--- files txime/main.js
+++ files txime/main.js
@@ -16,4 +16,63 @@ const setDate = () => {
   weekDate.textContent = date.toLocaleString("es", { weekday: "long" });
 };
 setDate();
-console.log("tuma");
+
+const taskForm = document.querySelector("form");
+const myTaskList = document.querySelector("ul");
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const taskObject = {
+    content: event.target.elements.newTask.value,
+    urgency: +event.target.elements.importance.value,
+    toDone: false,
+    taskDate: new Date().toLocaleString(),
+  };
+
+  addToList(taskObject);
+
+  taskForm.reset();
+
+  const tasksFromLocalStorage = getTasksFromLocalStorage();
+
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify([...tasksFromLocalStorage, taskObject])
+  );
+});
+
+const addToList = (taskObject) => {
+  const li = document.createElement("li");
+  const article = document.createElement("article");
+  const input = document.createElement("input");
+  const taskP = document.createElement("p");
+  const taskDateP = document.createElement("p");
+
+  taskP.textContent = taskObject.content;
+  if (taskObject.urgency) {
+    taskP.classList.add("important");
+  }
+  input.type = "checkbox";
+  taskDateP.textContent = taskObject.taskDate;
+
+  myTaskList.append(li);
+  li.append(article);
+  article.append(input, taskP, taskDateP);
+};
+
+const getTasksFromLocalStorage = () => {
+  const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  return tasksFromLocalStorage;
+};
+
+const generateTasksList = () => {
+  const tasksFromLocalStorage = getTasksFromLocalStorage();
+
+  for (const taskObject of tasksFromLocalStorage) {
+    addToList(taskObject);
+  }
+};
+
+generateTasksList();
